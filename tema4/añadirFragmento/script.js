3c805bb0b67b35f3e7c512650f8b73ca64df9b73
poliestr <poliestr@poliestr.com>
2023-01-31 00:27:03 +0100

    .

--- tema4/añadirFragmento/script.js
+++ tema4/añadirFragmento/script.js
@@ -1,5 +1,21 @@
 import { posts } from "./array.js"
 
-posts.forEach(element => {
-    document.getElementById('container').innerHTML += "TITULO  " + element.title + "<br>" + "CUERPO  " + element.body + "<br><br><br>";
-});
+const lista = document.getElementById('container')
+const fragment = document.createDocumentFragment();
+
+for ( const element of posts ){
+
+    const item = document.createElement('LI');
+    const titulo = document.createElement('H1');
+    const cuerpo = document.createElement('P');
+
+    titulo.textContent = element.title;
+    cuerpo.textContent = element.body;
+
+    item.appendChild(titulo);
+    item.appendChild(cuerpo);
+
+    fragment.appendChild(item);
+};
+
+lista.appendChild(fragment)
