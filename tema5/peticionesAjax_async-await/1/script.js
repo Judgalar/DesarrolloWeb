0b8a5dfdae722b6759e9421bf9c6f0b1ecd70d03
Juan Diego <juandiegogl24@gmail.com>
2023-04-14 12:31:25 +0200

    .

--- tema5/peticionesAjax_async-await/1/script.js
+++ tema5/peticionesAjax_async-await/1/script.js
@@ -1,98 +1,78 @@
-const tabla = document.getElementById('tabla');
+const contenedor = document.getElementById('contenedor');
 
-function verUsuarios(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => {
-        let html = `
-            <thead>
-                <tr>
-                <th scope="col">Name</th>
-                <th scope="col">UserName</th>
-                <th scope="col">Website</th>
-                </tr>
-            </thead>
-            <tbody id="cuerpo"></tbody>
-        `;
-        tabla.innerHTML = html;
-        let contenido = '';
-        data.forEach(usuario => {
-                contenido += `
-                <tr>
-                    <td>${usuario.name}</td>
-                    <td>${usuario.username}</td>
-                    <td>${usuario.website}</td>
-                    <td><button class="btn btn-secondary" onclick="verPosts(${usuario.id},'${usuario.username}')" >Ver posts</button></td>
-                </tr>
-            `;
-            cuerpo.innerHTML = contenido;
-        })
-    });
-}
-
-function verPosts(idUsuario,username){
-    fetch('https://jsonplaceholder.typicode.com/users/'+idUsuario+'/posts')
-    .then(res => res.json())
-    .then(data => {
-        let html = `
-            <thead>
-                <tr>
-                <th scope="col">PostTitle</th>
-                <th scope="col"><button class="btn btn-primary" onclick="verUsuarios()" >Volver</button></th>
-                </tr>
-            </thead>
-            <tbody id="cuerpo"></tbody>
-        `;
-        tabla.innerHTML = html;
-        let contenido = '';
-        data.forEach(post => {
+async function verUsuarios(){
+    const respuesta = await fetch('https://jsonplaceholder.typicode.com/users');
+    const usuarios = await respuesta.json();
+    
+    let contenido = '';
+    usuarios.forEach(usuario => {
             contenido += `
-                <tr>
-                    <td>${post.title}</td>
-                    <td><button class="btn btn-secondary" onclick="verPost(${post.id},'${username}')" >Ver post</button></td>
-                </tr>
-            `;
-            cuerpo.innerHTML = contenido;
-        })
-    });
-}
-
-function verPost(idPost,username){
-    fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
-    .then(res => res.json())
-    .then(post => {
-        let html = `
-            <thead>
-                <tr>
-                    <th scope="col">Username</th>
-                    <th scope="col">PostTitle</th>
-                    <th scope="col">PostBody</th>
-                    <th scope="col"><button class="btn btn-primary" onclick="verUsuarios()" >Volver</button></th>
-                </tr>
-            </thead>
-            <tbody id="cuerpo"></tbody>
+            <div class="elemento">
+                <ul>
+                    <li><b>Nombre:</b> ${usuario.name}</li>
+                    <li><b>Usuario:</b> ${usuario.username}</li>
+                    <li><b>Website:</b> ${usuario.website}</li>
+                </ul>
+                <button class="btn btn-secondary" onclick="verPosts(${usuario.id},'${usuario.username}')" >Ver posts</button>
+            </div>
         `;
-        tabla.innerHTML = html;
-        let contenido = `
-                <tr>
-                    <td>${username}</td>
-                    <td><h5>${post.title}</h5></td>
-                    <td>${post.body}</td>
-                </tr>
-            `;
-        cuerpo.innerHTML = contenido;
-
-        fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}/comments`)
-        .then(res => res.json())
-        .then(comentarios => {
-            let html = '<br><h1 style="color:green">Comentarios</h1><br><br>';
-            comentarios.forEach( comentario => {
-                html += `<h3>${comentario.name}</h3><br><p>${comentario.body}</p>`
-            })
-            cuerpo.innerHTML += html;
-        })
-    });
-
+    })
+    botonera.innerHTML = '';
+    postCont.innerHTML = '';
+    contenedor.innerHTML = contenido;
+    
 }
+
+async function verPosts(idUsuario,username){
+    const respuesta = await fetch('https://jsonplaceholder.typicode.com/users/'+idUsuario+'/posts')
+    const posts = await respuesta.json();
+    
+    let contenido = '';
+    posts.forEach(post => {
+        contenido += `
+            <div class="elemento">
+                <h3>${post.title}</h3>
+                <button class="btn btn-secondary" onclick="verPost(${post.id},'${username}')" >Ver post</button>
+            </div>
+        `;
+    })
+    botonera.innerHTML += '<button style="padding: 30px" onclick="verUsuarios()">Ver Usuarios</button>'
+    contenedor.innerHTML = contenido;
+}
+
+async function verPost(idPost,username){
+    const respuesta = await fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`);
+    const post = await respuesta.json();
+
+    let contenido = `
+        <ul style="background-color: white; padding: 20px">
+            <li><h2>${username}</h2></li>
+            <li><h3>${post.title}</h3></li>
+            <br>
+            <li>${post.body}</li>
+        </ul>
+    `;
+    contenedor.innerHTML='';
+    postCont.innerHTML = contenido;
+
+    const rComentarios = await fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}/comments`)
+    const comentarios = await rComentarios.json();
+
+    let html = '';
+    comentarios.forEach( comentario => {
+        html += `
+        <div class="elemento">
+            <h3>
+                ${comentario.name}
+            </h3>
+            <br>
+            <p>
+                ${comentario.body}
+            </p>
+        </div>`
+        
+    })
+    contenedor.innerHTML = html;
+ }
 
 verUsuarios();
--- tema5/peticionesAjax_async-await/2/script.js
+++ tema5/peticionesAjax_async-await/2/script.js
@@ -1,62 +1,61 @@
 // Función para obtener la lista de usuarios
-function getUsuarios() {
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(respuesta => respuesta.json())
-    .then(usuarios => {
-        let salida = '<h2>Lista de usuarios</h2>';
-        usuarios.forEach(user => {
-            salida += `
-            <ul>
-                <li>Nombre: ${user.name}</li>
-                <li>Usuario: ${user.username}</li>
-                <li>Sitio web: ${user.website}</li>
-                <button onclick="getTareas(${user.id})">Ver tareas</button>
-            </ul>
-            `;
-        });
-        document.getElementById('App').innerHTML = salida;
+async function getUsuarios() {
+    const respuesta = await fetch('https://jsonplaceholder.typicode.com/users')
+    const usuarios = await respuesta.json()
+
+    let salida = '<h2>Lista de usuarios</h2>';
+    usuarios.forEach(user => {
+        salida += `
+        <ul>
+            <li>Nombre: ${user.name}</li>
+            <li>Usuario: ${user.username}</li>
+            <li>Sitio web: ${user.website}</li>
+            <button onclick="getTareas(${user.id})">Ver tareas</button>
+        </ul>
+        `;
     });
+    document.getElementById('App').innerHTML = salida;
+
 }
 
 // Función para obtener las tareas de un usuario
-function getTareas(userId) {
-fetch(`https://jsonplaceholder.typicode.com/users/${userId}/todos`)
-    .then(respuesta => respuesta.json())
-    .then(tareas => {
-        let salida = '<h2>Lista de tareas</h2>';
-        let pendientes = [];
-        let completadas = [];
-        
-        tareas.forEach(tarea => {
-            if (tarea.completed) {
-            completadas.push(tarea);
-            } else {
-            pendientes.push(tarea);
-            }
-        });
-        
-        salida += '<h3>Tareas pendientes</h3>';
-        pendientes.forEach(tarea => {
-            salida += `
-            <ul>
-                <li>${tarea.title}</li>
-            </ul>
-            `;
-        });
-        
-        salida += '<h3>Tareas completadas</h3>';
-        completadas.forEach(tarea => {
-            salida += `
-            <ul>
-                <li>${tarea.title}</li>
-            </ul>
-            `;
-        });
-        
-        salida += '<button onclick="getUsuarios()">Volver a la lista de usuarios</button>';
-        document.getElementById('App').innerHTML = salida;
+async function getTareas(userId) {
+    const respuesta = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/todos`)
+    const tareas = await respuesta.json();
+
+    let salida = '<h2>Lista de tareas</h2>';
+    let pendientes = [];
+    let completadas = [];
+
+    tareas.forEach(tarea => {
+        if (tarea.completed) {
+        completadas.push(tarea);
+        } else {
+        pendientes.push(tarea);
+        }
     });
+
+    salida += '<h3>Tareas pendientes</h3>';
+    pendientes.forEach(tarea => {
+        salida += `
+        <ul>
+            <li>${tarea.title}</li>
+        </ul>
+        `;
+    });
+
+    salida += '<h3>Tareas completadas</h3>';
+    completadas.forEach(tarea => {
+        salida += `
+        <ul>
+            <li>${tarea.title}</li>
+        </ul>
+        `;
+    });
+
+    salida += '<button onclick="getUsuarios()">Volver a la lista de usuarios</button>';
+    document.getElementById('App').innerHTML = salida;
+ 
 }
 
-// Llamamos a la función para obtener la lista de usuarios al cargar la página
 window.onload = getUsuarios;
