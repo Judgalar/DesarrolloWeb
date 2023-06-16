1ae1560d205770963694df0d13e5962446738905
Juan Diego García Lara <juandiegogl24@gmail.com>
2023-06-16 21:35:05 +0200

    .

--- tema4/7_accesoCamposFormulario/accesoACampos.js
+++ tema4/7_accesoCamposFormulario/accesoACampos.js
@@ -1,29 +1,22 @@
-
-const botones = document.querySelectorAll(' [type="Submit"],[type="button"]');
-
-botones.forEach( boton =>
-    boton.addEventListener('click', function(event){
+document.body.addEventListener('click', function (event) {
+    // Verificar si el elemento es un submit o un button
+    if (event.target.matches('[type="submit"], [type="button"]')) {
         event.preventDefault();
-    
+
         // Obtener los campos del formulario
-        let campos = Array.from(document.querySelectorAll('input, textarea, select,datalist'));
-    
-        // Crear una cadena con los nombres y valores
-        let datosUsuario = "";
-    
-        campos.forEach(function (element){
+        let campos = Array.from(document.querySelectorAll('input, textarea, select, datalist'));
+
+        let datosUsuario = campos.map(function (element) {
             campoNombre = element.getAttribute('name');
-            if(campoNombre == null) campoNombre = element.getAttribute('id');
-            if(campoNombre == null) campoNombre = element.getAttribute('type');
+            if (campoNombre == null) campoNombre = element.getAttribute('id');
+            if (campoNombre == null) campoNombre = element.getAttribute('type');
             campoValor = element.value;
-            datosUsuario += campoNombre + ": " + campoValor + "<br>";
-        });
-    
+            return campoNombre + ": " + campoValor;
+        }).join("<br>");
+
         // Mostrar la cadena en el div con id="valores"
         document.getElementById("valores").innerHTML = datosUsuario;
-    })
-);
-
-
+    }
+});
 
 
