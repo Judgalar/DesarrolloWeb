997f9c3d098ec6c4d4ce444b702f2128cdbcd1b0
Juan Diego García Lara <juandiegogl24@gmail.com>
2023-06-16 22:43:45 +0200

    paginacion

--- tema5/app-APIGOREST/src/ListaUsuarios.jsx
+++ tema5/app-APIGOREST/src/ListaUsuarios.jsx
@@ -1,18 +1,19 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import Usuario from './Usuario'
 import './ListaUsuarios.css'
 import './loading.css'
-import { setApiKey , TOKEN} from './apiKey.js'
+import { setApiKey, TOKEN } from './apiKey.js'
 
-function ListaUsuarios () { 
+function ListaUsuarios() {
     const [usuarios, setUsuarios] = useState(null)
     const [eliminandoUsuario, setEliminandoUsuario] = useState(false);
+    const [pagina, setPagina] = useState(1);
 
-    useEffect(() => { getUsers() } , []);
+    useEffect(() => { getUsers() }, []);
 
     const getUsers = async () => {
         try {
-            const response = await fetch('https://gorest.co.in/public/v2/users', {
+            const response = await fetch(`https://gorest.co.in/public/v2/users?page=${pagina}&per_page=5`, {
                 method: 'GET',
                 headers: {
                     'Authorization': 'Bearer ' + TOKEN,
@@ -39,7 +40,7 @@ function ListaUsuarios () {
                     'Accept': 'application/json'
                 },
             });
-            if (response.ok) { 
+            if (response.ok) {
                 getUsers(); // Actualizamos la lista de usuarios 
             } else {
                 console.log('Error eliminando usuario');
@@ -54,31 +55,50 @@ function ListaUsuarios () {
         getUsers();
     }
 
-    return(
+    const handlePreviousPage = () => {
+        setPagina((actual) => --actual);
+        console.log('Pagina ' + pagina)
+        getUsers()
+    }
+    const handleNextPage = () => {
+        setPagina((actual) => ++actual);
+        console.log('Pagina ' + pagina)
+        getUsers()
+    }
+
+    return (
         <>
             <h1>Lista de Usuarios</h1>
             {eliminandoUsuario && <ul className="loading"><li></li><li></li><li></li></ul>}
             {usuarios ? (
-                <div className="usuarios">
-                    { usuarios.map((usuario) => (
-                          <Usuario
-                            key={usuario.id}
-                            userId={usuario.id}
-                            name={usuario.name}
-                            email={usuario.email}
-                            gender={usuario.gender}
-                            status={usuario.status}
-                            onDelete={handleDeleteUser}
-                            updateList={updateList}
-                          />
-                        ))             
-                    }
-                </div>
+                <>
+                    <div className="usuarios">
+                        {usuarios.map((usuario) => (
+                            <Usuario
+                                key={usuario.id}
+                                userId={usuario.id}
+                                name={usuario.name}
+                                email={usuario.email}
+                                gender={usuario.gender}
+                                status={usuario.status}
+                                onDelete={handleDeleteUser}
+                                updateList={updateList}
+                            />
+                        ))
+                        }
+                    </div>
+                    <div className="paginacion">
+                        <button onClick={handlePreviousPage} disabled={pagina === 1}>
+                            Página anterior
+                        </button>
+                        <button onClick={handleNextPage}>Página siguiente</button>
+                    </div>
+                </>
             ) : (
                 <ul className="loading"><li></li><li></li><li></li></ul>
             )}
         </>
     )
 }
-  
+
 export default ListaUsuarios
